effe7fd0d4380893ef77729cafe8f0e9436df745
Daniel Nguyen <daniel.nguyen@mrjaffesclass.com>
2023-08-24 12:02:25 -0700

    Initial Commit

---
+++ HelloWorld.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class HelloWorld here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class HelloWorld
+{
+
+
+    /**
+     * Constructor for objects of class HelloWorld
+     */
+    public HelloWorld()
+    {
+        System.out.println("Welcome back, hello world!!!");
+    }
+
+
+}
